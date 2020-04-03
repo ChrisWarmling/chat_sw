08156eede3d42442a1d5961a0df33fe59a7a5baf
Christian <christianwarmling@gmail.com>
2020-04-03 16:46:45 -0300

    fim

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -4,14 +4,19 @@ import './app.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 function App() {
-  function add() {
+  function sendMsg() {
+    var msg = document.getElementById('msg')
+    var textMsg = document.createTextNode(`${msg.value}`)
+    if(msg.value === '') {alert('Escreva Algo')}
+    else{
     var element = document.getElementById('element')
-    var test = document.createElement('h3')
-    var texto = document.getElementById('texto')
-    var text = document.createTextNode(`${texto.value}`)
-    console.log(texto.value)
-    test.appendChild(text)
-    element.appendChild(test)
+    var spanMsg = document.createElement('span')
+    console.log(msg.value)
+    spanMsg.appendChild(textMsg)
+    element.appendChild(spanMsg)}
+    msg.value = ''
+    var rolar = document.getElementById("chatbox-scroll");
+      rolar.scrollTop = rolar.scrollHeight;
   }
   return (
     <div >
@@ -19,20 +24,16 @@ function App() {
         <div className='chatbox-title'>
           <span>ChatBot</span>
         </div>
-        <div id='element' className='chatbox-chat'>
+        <div id='chatbox-scroll' className='chatbox-chat'>
           <div className='chatbox-bot'>
             <span>Olá, em que posso ajudar?</span>
           </div>
-          <div className='chatbox-user'>
-            <span>Olá, em que posso ajudar?</span>
-          </div>
-          <div className='chatbox-user'>
-            <span>Olá</span>
+          <div id='element' className='chatbox-user'>
           </div>
         </div>
         <div className='chatbox-send'>
-          <TextareaAutosize className='chatbox-input' />
-          <i className='fa fa-paper-plane fa-lg'></i>
+          <TextareaAutosize id='msg' className='chatbox-input' />
+          <i className={`fa fa-paper-plane fa-lg`} onClick={sendMsg}></i>
         </div>
       </div>
     </div>
